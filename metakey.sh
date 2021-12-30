6db7ec40b8ffd3583f98b1e8436ea056dfde4c4d
kuan47 <96853825+kuan47@users.noreply.github.com>
2021-12-30 08:38:54 +0530

    Create metakey.sh

---
+++ metakey.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,org.kde.kglobalaccel.Component,invokeShortcut,ExposeAll" && qdbus org.kde.KWin /KWin reconfigure
